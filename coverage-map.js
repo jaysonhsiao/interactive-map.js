2477b0b1b15f8c9715480b8ed8490116ffb460c8
jaysonhsiao <88578742+jaysonhsiao@users.noreply.github.com>
2021-08-10 11:25:21 +1000

    Add files via upload

---
+++ coverage-map.js
@@ -0,0 +1,328 @@
+var svgMap = new svgMap({
+  targetElementID: 'svgMap',
+  data: {
+    data: {
+      un_other: {},
+      idv:{}
+    },
+    applyData: 'un_other',
+    values: {
+      AF: { un_other : 0, idv : 0 },
+      AX: { un_other : 1, idv : 0 },
+      AL: { un_other : 0, idv : 0 },
+      DZ: { un_other : 0, idv : 0 },
+      AS: { un_other : 1, idv : 0 },
+      AD: { un_other : 0, idv : 0 },
+      AO: { un_other : 1, idv : 0 },
+      AI: { un_other : 1, idv : 0 },
+      AQ: { un_other : 1, idv : 0 },
+      AG: { un_other : 0, idv : 0 },
+      AR: { un_other : 0, idv : 0 },
+      AM: { un_other : 1, idv : 0 },
+      AW: { un_other : 0, idv : 0 },
+      AU: { un_other : 0, idv : 1 },
+      AT: { un_other : 0, idv : 1 },
+      AZ: { un_other : 0, idv : 0 },
+      BS: { un_other : 0, idv : 0 },
+      BH: { un_other : 0, idv : 0 },
+      BD: { un_other : 0, idv : 0 },
+      BB: { un_other : 1, idv : 0 },
+      BY: { un_other : 1, idv : 0 },
+      BE: { un_other : 0, idv : 0 },
+      BZ: { un_other : 0, idv : 0 },
+      BJ: { un_other : 0, idv : 0 },
+      BM: { un_other : 0, idv : 0 },
+      BT: { un_other : 1, idv : 0 },
+      BO: { un_other : 1, idv : 0 },
+      BA: { un_other : 1, idv : 0 },
+      BW: { un_other : 0, idv : 0 },
+      BR: { un_other : 0, idv : 1 },
+      IO: { un_other : 1, idv : 0 },
+      VG: { un_other : 0, idv : 0 },
+      BN: { un_other : 1, idv : 0 },
+      BG: { un_other : 0, idv : 0 },
+      BF: { un_other : 1, idv : 0 },
+      BI: { un_other : 1, idv : 0 },
+      KH: { un_other : 1, idv : 0 },
+      CM: { un_other : 0, idv : 0 },
+      CA: { un_other : 0, idv : 1 },
+      CV: { un_other : 1, idv : 0 },
+      BQ: { un_other : 1, idv : 0 },
+      KY: { un_other : 0, idv : 1 },
+      CF: { un_other : 1, idv : 0 },
+      TD: { un_other : 1, idv : 0 },
+      CL: { un_other : 0, idv : 0 },
+      CN: { un_other : 0, idv : 1 },
+      CX: { un_other : 1, idv : 0 },
+      CC: { un_other : 1, idv : 0 },
+      CO: { un_other : 0, idv : 0 },
+      KM: { un_other : 1, idv : 0 },
+      CG: { un_other : 0, idv : 0 },
+      CK: { un_other : 1, idv : 0 },
+      CR: { un_other : 0, idv : 0 },
+      HR: { un_other : 1, idv : 0 },
+      CU: { un_other : 1, idv : 0 },
+      CW: { un_other : 1, idv : 0 },
+      CY: { un_other : 0, idv : 0 },
+      CZ: { un_other : 0, idv : 0 },
+      CD: { un_other : 1, idv : 0 },
+      DK: { un_other : 0, idv : 1 },
+      DJ: { un_other : 1, idv : 0 },
+      DM: { un_other : 1, idv : 0 },
+      DO: { un_other : 0, idv : 1 },
+      EC: { un_other : 0, idv : 0 },
+      EG: { un_other : 0, idv : 0 },
+      SV: { un_other : 1, idv : 0 },
+      GQ: { un_other : 1, idv : 0 },
+      ER: { un_other : 1, idv : 0 },
+      EE: { un_other : 0, idv : 0 },
+      ET: { un_other : 0, idv : 0 },
+      FK: { un_other : 1, idv : 0 },
+      FO: { un_other : 1, idv : 0 },
+      FM: { un_other : 1, idv : 0 },
+      FJ: { un_other : 1, idv : 0 },
+      FI: { un_other : 0, idv : 1 },
+      FR: { un_other : 0, idv : 1 },
+      GF: { un_other : 1, idv : 0 },
+      PF: { un_other : 1, idv : 0 },
+      TF: { un_other : 1, idv : 0 },
+      GA: { un_other : 0, idv : 0 },
+      GM: { un_other : 0, idv : 0 },
+      GE: { un_other : 0, idv : 0 },
+      DE: { un_other : 0, idv : 1 },
+      GH: { un_other : 0, idv : 0 },
+      GI: { un_other : 0, idv : 0 },
+      GR: { un_other : 0, idv : 0 },
+      GL: { un_other : 1, idv : 0 },
+      GD: { un_other : 1, idv : 0 },
+      GP: { un_other : 1, idv : 0 },
+      GU: { un_other : 1, idv : 0 },
+      GT: { un_other : 1, idv : 0 },
+      GN: { un_other : 1, idv : 0 },
+      GW: { un_other : 1, idv : 0 },
+      GY: { un_other : 1, idv : 0 },
+      HT: { un_other : 1, idv : 0 },
+      HN: { un_other : 0, idv : 0 },
+      HK: { un_other : 0, idv : 0 },
+      HU: { un_other : 0, idv : 0 },
+      IS: { un_other : 1, idv : 0 },
+      IN: { un_other : 0, idv : 1 },
+      ID: { un_other : 0, idv : 0 },
+      IR: { un_other : 0, idv : 0 },
+      IQ: { un_other : 0, idv : 0 },
+      IE: { un_other : 0, idv : 0 },
+      IM: { un_other : 1, idv : 0 },
+      IL: { un_other : 0, idv : 0 },
+      IT: { un_other : 0, idv : 1 },
+      CI: { un_other : 0, idv : 0 },
+      JM: { un_other : 0, idv : 0 },
+      JP: { un_other : 0, idv : 0 },
+      JE: { un_other : 1, idv : 0 },
+      JO: { un_other : 0, idv : 0 },
+      KZ: { un_other : 0, idv : 0 },
+      KE: { un_other : 0, idv : 0 },
+      KI: { un_other : 1, idv : 0 },
+      XK: { un_other : 1, idv : 0 },
+      KW: { un_other : 0, idv : 0 },
+      KG: { un_other : 0, idv : 0 },
+      LA: { un_other : 1, idv : 0 },
+      LV: { un_other : 0, idv : 0 },
+      LB: { un_other : 0, idv : 0 },
+      LS: { un_other : 1, idv : 0 },
+      LR: { un_other : 1, idv : 0 },
+      LY: { un_other : 0, idv : 0 },
+      LI: { un_other : 1, idv : 0 },
+      LT: { un_other : 0, idv : 0 },
+      LU: { un_other : 0, idv : 0 },
+      MO: { un_other : 1, idv : 0 },
+      MK: { un_other : 1, idv : 0 },
+      MG: { un_other : 1, idv : 0 },
+      MW: { un_other : 0, idv : 0 },
+      MY: { un_other : 0, idv : 0 },
+      MV: { un_other : 1, idv : 0 },
+      ML: { un_other : 1, idv : 0 },
+      MT: { un_other : 0, idv : 0 },
+      MH: { un_other : 1, idv : 0 },
+      MQ: { un_other : 1, idv : 0 },
+      MR: { un_other : 0, idv : 0 },
+      MU: { un_other : 0, idv : 0 },
+      YT: { un_other : 1, idv : 0 },
+      MX: { un_other : 0, idv : 1 },
+      MD: { un_other : 1, idv : 0 },
+      MC: { un_other : 1, idv : 0 },
+      MN: { un_other : 1, idv : 0 },
+      ME: { un_other : 1, idv : 0 },
+      MS: { un_other : 1, idv : 0 },
+      MA: { un_other : 0, idv : 0 },
+      MZ: { un_other : 1, idv : 0 },
+      MM: { un_other : 1, idv : 0 },
+      NA: { un_other : 1, idv : 0 },
+      NR: { un_other : 1, idv : 0 },
+      NP: { un_other : 0, idv : 0 },
+      NL: { un_other : 0, idv : 1 },
+      NC: { un_other : 1, idv : 0 },
+      NZ: { un_other : 0, idv : 1 },
+      NI: { un_other : 0, idv : 0 },
+      NE: { un_other : 1, idv : 0 },
+      NG: { un_other : 0, idv : 0 },
+      NU: { un_other : 1, idv : 0 },
+      NF: { un_other : 1, idv : 0 },
+      KP: { un_other : 1, idv : 0 },
+      MP: { un_other : 1, idv : 0 },
+      NO: { un_other : 0, idv : 1 },
+      OM: { un_other : 0, idv : 0 },
+      PK: { un_other : 0, idv : 0 },
+      PW: { un_other : 1, idv : 0 },
+      PS: { un_other : 0, idv : 0 },
+      PA: { un_other : 0, idv : 0 },
+      PG: { un_other : 1, idv : 0 },
+      PY: { un_other : 1, idv : 0 },
+      PE: { un_other : 0, idv : 0 },
+      PH: { un_other : 0, idv : 0 },
+      PN: { un_other : 1, idv : 0 },
+      PL: { un_other : 0, idv : 0 },
+      PT: { un_other : 0, idv : 0 },
+      PR: { un_other : 1, idv : 0 },
+      QA: { un_other : 0, idv : 0 },
+      RE: { un_other : 1, idv : 0 },
+      RO: { un_other : 0, idv : 0 },
+      RU: { un_other : 0, idv : 0 },
+      RW: { un_other : 0, idv : 0 },
+      SH: { un_other : 1, idv : 0 },
+      KN: { un_other : 1, idv : 0 },
+      LC: { un_other : 1, idv : 0 },
+      PM: { un_other : 1, idv : 0 },
+      VC: { un_other : 1, idv : 0 },
+      WS: { un_other : 1, idv : 0 },
+      SM: { un_other : 1, idv : 0 },
+      ST: { un_other : 0, idv : 0 },
+      SA: { un_other : 0, idv : 0 },
+      SN: { un_other : 0, idv : 0 },
+      RS: { un_other : 1, idv : 0 },
+      SC: { un_other : 0, idv : 0 },
+      SL: { un_other : 0, idv : 0 },
+      SG: { un_other : 0, idv : 0 },
+      SX: { un_other : 1, idv : 0 },
+      SK: { un_other : 0, idv : 0 },
+      SI: { un_other : 0, idv : 0 },
+      SB: { un_other : 1, idv : 0 },
+      SO: { un_other : 0, idv : 0 },
+      ZA: { un_other : 0, idv : 1 },
+      GS: { un_other : 1, idv : 0 },
+      KR: { un_other : 0, idv : 0 },
+      SS: { un_other : 1, idv : 0 },
+      ES: { un_other : 0, idv : 1 },
+      LK: { un_other : 0, idv : 0 },
+      SD: { un_other : 0, idv : 0 },
+      SR: { un_other : 1, idv : 0 },
+      SJ: { un_other : 1, idv : 0 },
+      SZ: { un_other : 1, idv : 0 },
+      SE: { un_other : 0, idv : 1 },
+      CH: { un_other : 0, idv : 1 },
+      SY: { un_other : 0, idv : 0 },
+      TW: { un_other : 0, idv : 0 },
+      TJ: { un_other : 0, idv : 0 },
+      TZ: { un_other : 0, idv : 0 },
+      TH: { un_other : 0, idv : 0 },
+      TL: { un_other : 1, idv : 0 },
+      TG: { un_other : 0, idv : 0 },
+      TK: { un_other : 1, idv : 0 },
+      TO: { un_other : 1, idv : 0 },
+      TT: { un_other : 1, idv : 0 },
+      TN: { un_other : 0, idv : 0 },
+      TR: { un_other : 0, idv : 0 },
+      TM: { un_other : 1, idv : 0 },
+      TC: { un_other : 1, idv : 0 },
+      TV: { un_other : 1, idv : 0 },
+      UG: { un_other : 0, idv : 0 },
+      UA: { un_other : 0, idv : 0 },
+      AE: { un_other : 0, idv : 0 },
+      GB: { un_other : 0, idv : 1 },
+      US: { un_other : 0, idv : 1 },
+      UM: { un_other : 1, idv : 0 },
+      VI: { un_other : 1, idv : 0 },
+      UY: { un_other : 0, idv : 0 },
+      UZ: { un_other : 1, idv : 0 },
+      VU: { un_other : 1, idv : 0 },
+      VA: { un_other : 1, idv : 0 },
+      VE: { un_other : 0, idv : 0 },
+      VN: { un_other : 0, idv : 0 },
+      WF: { un_other : 1, idv : 0 },
+      EH: { un_other : 1, idv : 0 },
+      YE: { un_other : 1, idv : 0 },
+      ZM: { un_other : 0, idv : 0 },
+      ZW: { un_other : 0, idv : 0 }
+    }
+  },
+  colorMin: '#3ba5f6',
+  colorMax: '#93cffd',
+  colorNoData: '#E2E2E2',
+  thresholdMax: 1,
+  thresholdMin: 0,
+  initialZoom: 1.06,
+  initialPan: {
+    x: 420,
+    y: 50
+  },
+  onGetTooltip: function (tooltipDiv, countryID, countryValues) {
+    // Geting the list of countries
+    var countries = svgMap.countries;
+
+    // Create tooltip content element
+    var tooltipContentElement = document.createElement('div');
+    tooltipContentElement.style.padding = '10px 15px';
+
+
+    if (countryValues && countryValues.un_other == 1) {
+      var innerHTML =
+        '<div style="display:flex; flex-direction: column; align-items: center;justify-content: left; align-items: flex-start">' +
+        '<div style="display:flex; flex-direction: row; align-items: center;justify-content: left; margin-bottom: 8px">' +
+        '<img src="https://uploads-ssl.webflow.com/60f75fe95d9a237d3f504b14/610fee8d50ce77a013548f41_un.svg" style="width:24px; height: 18px;margin-right: 8px; border: 2px solid #ddd; display: block;" alt=""> <span style="margin-right: 8px; font-weight: bold; font-family: Nunito Sans;font-size: 12px;font-size: 16px;  line-height: 20px;">UN</span></div>';
+    } else {
+      var innerHTML =""; }
+
+
+    // Fill content
+    innerHTML += '<div style="display:flex; align-items: center;justify-content: left;">' +
+      '<img src="https://cdn.jsdelivr.net/gh/hjnilsson/country-flags@latest/svg/' + countryID.toLowerCase() + '.svg" style="width:24px; height: 18px;margin-right: 8px; border: 2px solid #ddd; display: block;" alt="">';
+
+
+
+
+    innerHTML += '<div style="margin-right: 8px; font-weight: bold; font-family: Nunito Sans;font-size: 12px;font-size: 16px;  line-height: 20px;">' +
+      countries[countryID] +
+      '</div><img src="https://uploads-ssl.webflow.com/60f75fe95d9a237d3f504b14/610fea4361ddcb0ad3012a50_label_important_24px.svg"></div>'
+
+
+    if (countryValues && countryValues.un_other == 1) {
+      innerHTML += '</div>';
+    }
+
+
+    //       innerHTML += '<hr>';
+
+
+    if (countryValues && countryValues.idv == 1) {
+      innerHTML +=
+        '<hr>'+
+        '<div class="icon-svg"><span style="margin-right: 8px"><img src="https://uploads-ssl.webflow.com/60f75fe95d9a237d3f504b14/610e768a90588ca60bc5de28_check_24px.svg"></span>PEP/SIP and Sanctions Screening</div>' +
+        '<div class="icon-svg"><span style="margin-right: 8px"><img src="https://uploads-ssl.webflow.com/60f75fe95d9a237d3f504b14/610e768a90588ca60bc5de28_check_24px.svg"></span>Adverse Media Screening</div>' +
+        '<div class="icon-svg"><span style="margin-right: 8px"><img src="https://uploads-ssl.webflow.com/60f75fe95d9a237d3f504b14/610e768a90588ca60bc5de28_check_24px.svg"></span>Enhanced Due Diligence</div>' +
+        '<div class="icon-svg"><span style="margin-right: 8px"><img src="https://uploads-ssl.webflow.com/60f75fe95d9a237d3f504b14/610e768a90588ca60bc5de28_check_24px.svg"></span>ID Verification</div>'
+      ;
+    } else {
+      innerHTML +=
+        '<hr>'+
+        '<div class="icon-svg"><span style="margin-right: 8px"><img src="https://uploads-ssl.webflow.com/60f75fe95d9a237d3f504b14/610e768a90588ca60bc5de28_check_24px.svg"></span>PEP/SIP and Sanctions Screening</div>' +
+        '<div class="icon-svg"><span style="margin-right: 8px"><img src="https://uploads-ssl.webflow.com/60f75fe95d9a237d3f504b14/610e768a90588ca60bc5de28_check_24px.svg"></span>Adverse Media Screening</div>' +
+        '<div class="icon-svg"><span style="margin-right: 8px"><img src="https://uploads-ssl.webflow.com/60f75fe95d9a237d3f504b14/610e768a90588ca60bc5de28_check_24px.svg"></span>Enhanced Due Diligence</div>'
+      ;
+    }
+
+
+
+    // Return element with custom content
+    tooltipContentElement.innerHTML = innerHTML;
+    return tooltipContentElement;
+  }
+});
